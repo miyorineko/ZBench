6b65a289b2bf0cf3b4bdf57b9f8dce74f309aa8c
miyorineko <miyorineko@users.noreply.github.com>
2018-03-11 15:06:43 +0900

    Update ZBench-TC.sh

--- ZBench-TC.sh
+++ ZBench-TC.sh
@@ -355,7 +355,7 @@ NetPiSM=$( sed -n "24p" /tmp/speed_tc.txt )
 NetUPCM=$( sed -n "25p" /tmp/speed_tc.txt )
 NetDWCM=$( sed -n "26p" /tmp/speed_tc.txt )
 NetPiCM=$( sed -n "27p" /tmp/speed_tc.txt )
-wget -N --no-check-certificate https://raw.githubusercontent.com/FunctionClub/ZBench/master/Generate.py >> /dev/null 2>&1
+wget -N --no-check-certificate https://raw.githubusercontent.com/miyorineko/ZBench/master/Generate.py >> /dev/null 2>&1
 python Generate.py && rm -rf Generate.py && cp /root/report.html /tmp/report/index.html
 TSM=$( cat /tmp/shm.txt_table )
 TST=$( cat /tmp/sht.txt_table )
